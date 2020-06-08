fe0f3fa043fe6e0e2f47f7e1de94de63cc512033
V-Kwong <vincent_kwong578@hotmail.com>
2020-06-07 21:28:34 -0700

    added page component

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,30 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+import Page from '../components/page';
+
+const IndexPage = () => {
+  const divStyle = {
+    background: 'linear-gradient(141deg, #d32f2f 0%, #bb2f2f 51%, #922f2f 75%)'
+  };
+  return (
+    <div style={divStyle}>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      <Page>Majungasaurus</Page>
+      <Page>Apatosaurus</Page>
+      <Page>Brontosaurus</Page>
+      <Page>Brachiosaurus</Page>
+      <Page>Allosaurus</Page>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+  )
+}
 
 export default IndexPage
