1d538394059085cffbb75a234f580fb4e0adcfa4
V-Kwong <vincent_kwong578@hotmail.com>
2020-06-08 20:49:05 -0700

    added gatsby-images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
@@ -9,11 +11,25 @@ import Page from '../components/page';
 
 const IndexPage = () => {
   const divStyle = {
-    background: 'linear-gradient(141deg, #d32f2f 0%, #bb2f2f 51%, #922f2f 75%)'
+    background: 'linear-gradient(141deg, #d32f2f 0%, #bb2f2f 51%, #922f2f 75%)',
+    maxWidth: '400',
   };
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "black-dinosaur-logo-clip-art-png-clip-art.png" }) {
+        childImageSharp {
+          # Specify a fluid image and fragment
+          # The default maxWidth is 800 pixels
+          fluid(maxWidth: 400) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
   return (
     <div style={divStyle}>
-      <SEO title="Home" />
+      {/* <SEO title="Home" />
       <h1>Hi people</h1>
       <p>Welcome to your new Gatsby site.</p>
       <p>Now go build something great.</p>
@@ -21,12 +37,19 @@ const IndexPage = () => {
         <Image />
       </div>
       <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-      <Page>Majungasaurus</Page>
-      <Page>Apatosaurus</Page>
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
+      <Page>
+        <div style={divStyle}>
+          <Img
+            fluid={data.file.childImageSharp.fluid}
+            alt="Gatsby Docs are awesome"
+          />
+        </div>
+      </Page>
+      {/* <Page>Apatosaurus</Page>
       <Page>Brontosaurus</Page>
       <Page>Brachiosaurus</Page>
-      <Page>Allosaurus</Page>
+      <Page>Allosaurus</Page> */}
     </div>
   )
 }
