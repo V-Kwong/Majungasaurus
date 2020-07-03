44f3c3edf2b727a60755541892bef1ce9d8b379c
Vincent Kwong <vincent_kwong578@hotmail.com>
2020-07-02 18:18:53 -0700

    finished simple hover button

--- src/components/discoverButton.js
+++ src/components/discoverButton.js
@@ -10,14 +10,13 @@ export const DiscoverButton = styled.a`
   cursor: pointer;
   position: relative;
   transition: all 0.4s cubic-bezier(0.42, 0, 0.58, 1);
-  border: 2px solid #fff;
+  border: 2px solid #ffe001;
   -webkit-border-radius: 5px;
   border-radius: 5px;
   -moz-border-radius: 5px;
 
   &:hover {
-    // color: #000 !important;
-    background-color: transparent;
-    border: 2px solid #ffe001;
+    color: #000;
+    background-color: #ffe001;
   }
 `
