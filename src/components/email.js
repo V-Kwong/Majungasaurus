5941319d08d9a9d06c4c3ee8de5cd6270cb086ff
Vincent Kwong <vincent_kwong578@hotmail.com>
2020-07-03 20:09:08 -0700

    added email component

---
+++ src/components/email.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Email = styled.a`
+  text-decoration: none;
+  background: transparent;
+  border: none;
+  margin: 0;
+  padding: 0;
+  cursor: pointer;
+  transition: all 0.5s ease;
+
+  &:hover {
+    opacity: 0.75;
+  }
+`
