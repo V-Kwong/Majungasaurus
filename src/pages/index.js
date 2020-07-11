1c912ba4818126497cc27b2b8821a44dd3f0bd5f
Vincent Kwong <vincent_kwong578@hotmail.com>
2020-07-10 22:29:44 -0700

    more logo button sticky edits

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,34 +36,18 @@ export default class IndexPage extends Component {
       <Scrollbars 
         style={{ width: '100vw', height: '100vh' }}
       >
-        {/* <SEO title="Home" />
-        <h1>Hi people</h1>
-        <p>Welcome to your new Gatsby site.</p>
-        <p>Now go build something great.</p>
-        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-          <Image />
-        </div>
-        <Link to="/page-2/">Go to page 2</Link> <br />
-        <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
+
         <Page>
           <Main />
         </Page>
-        {/* <Page>
-          <h1 style={{ color: 'white' }}>Apatosaurus</h1>
-        </Page>
-        <Page>
-          <h1 style={{ color: 'white' }}>Brontosaurus</h1>
-        </Page>
-        <Page>
-          <h1 style={{ color: 'white' }}>Brachiosaurus</h1>
-        </Page> */}
+      
         <LogoButton onClick={this.scrollToTop}>
           <img src={Logo} className={styles.logo} alt=""/>
         </LogoButton>
         <Page>
           <About />
         </Page>
-        <Page>
+        {/* <Page>
           <Product
             title='Brachiosaurus'
             text='Most Iconic and initially thought to be one of the Largest Dinosaurs.'
@@ -77,7 +61,7 @@ export default class IndexPage extends Component {
         </Page>
         <Page>
           <Contact />
-        </Page>
+        </Page> */}
       </Scrollbars>
     )
   }
